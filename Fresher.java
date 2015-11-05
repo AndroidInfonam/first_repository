bc01949dd1e2241ab3158ad9c4cdbd10e46a49d8
thoaibui090ifnam <thoai.bui@infonam.com>
2015-11-05 09:54:14 +0700

    Edit Fresher.java add new method

--- Fresher.java
+++ Fresher.java
@@ -2,4 +2,9 @@ public class Fresher{
 	public static void main(String[] args){
 	
 	}
+
+	//Thoai edit: created a new method
+	public void printName() {
+		system.out.println("Bui Thanh Thoai");
+	}
 }
