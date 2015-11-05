57dc1f40c121663c450dc1dbdef7e829d868be31
chqthang <thang.cai@infonam.com>
2015-11-05 13:31:46 +0700

    no message

--- Fresher.java
+++ Fresher.java
@@ -1,5 +1,8 @@
 public class Fresher{
 	public static void main(String[] args){
+
+		System.out.println("Hello world!");
+
  public String getFeshers{
 return Thang;
 }
@@ -7,11 +10,17 @@ return Thang;
 	//Thoai edit: created a new method
 	public String getFeshers() {
 		return "Thoai";
+		System.out.println("Hello Thoai commit 1!");
+
 	}
 	
        public  void outname()
 	{
+
 		System.out.println("thang");
+
+		System.out.println("Commit 2");
+
 	}
 
 	 public String getFeshers{
@@ -19,11 +28,19 @@ return Thang;
 	}
 
 	//Thoai edit: created a new method
+
 	public void printName() {
 		system.out.println("Bui Thanh Thoai");
 	public String getFeshers() {
 
 	public String getFeshers {
+
+	public String getFeshers() {
+		return "Thoai";
+	}
+
+	//Thoai edit: created a new method
+	public String getFeshers() {
 		return "Thoai";
 	}
 }
