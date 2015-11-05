7131e8b650b3befa416fb763a3a6ea125c32f6cb
Neil.Tran <neil.tran@infonam.com>
2015-11-05 13:22:29 +0700

    [Thuan] add method checkId()

--- Fresher.java
+++ Fresher.java
@@ -1,24 +1,29 @@
 public class Fresher{
-	public static void main(String[] args){
-		System.out.println("Hello world!");
-	}
+		public static void main(String[] args){
+			System.out.println("Hello world!");
+		}
 	
-       public  void outname()
-	{
-		System.out.println("thang");
-	}
+       		public  void outname()
+		{
+			System.out.println("thang");
+		}
 
-	 public String getFeshers{
-		return Thang;
-	}
+	 	public String getFeshers{
+			return Thang;
+		}
 
-	//Thoai edit: created a new method
-	public String getFeshers() {
-		return "Thoai";
-	}
+		//Thoai edit: created a new method
+		public String getFeshers {
+			return "Thoai";
+		}
 
-	//Thoai edit: created a new method
-	public String getFeshers() {
-		return "Thoai";
+		//Thoai edit: created a new method
+		public String getFeshers {
+			return "Thoai";
+		}
+
+		public boolean checkId(String id){
+			return false;
+		}
 	}
 }
