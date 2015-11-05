cd9d1251c61ef6411d20194db5a7c9e21db363c6
thoaibui090ifnam <thoai.bui@infonam.com>
2015-11-05 10:14:25 +0700

    add method to get Fresher name

--- Fresher.java
+++ Fresher.java
@@ -1,8 +1,11 @@
 public class Fresher{
 	public static void main(String[] args){
-
  public String getFeshers{
 return Thang;
 }
+
+	//Thoai edit: created a new method
+	public String getFeshers {
+		return "Thoai";
 	}
 }
